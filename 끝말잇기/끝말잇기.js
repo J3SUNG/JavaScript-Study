4d65de0920e5191d020d83f3c7f1abb30630dcd0
J3SUNG <wptjd6141@naver.com>
2020-05-26 20:00:41 +0900

    Update js study

--- 끝말잇기/끝말잇기.js
+++ 끝말잇기/끝말잇기.js
@@ -1,5 +1,6 @@
 var body = document.body;
 var word = document.createElement('div');
+var form = document.createElement('form');
 var input = document.createElement('input');
 var button = document.createElement('button');
 var result = document.createElement('div');
@@ -7,16 +8,21 @@ var result = document.createElement('div');
 word.textContent = '자바스크립트';
 button.textContent = '입력';
 document.body.append(word);
-document.body.append(input);
-document.body.append(button);
+document.body.append(form);
+form.append(input);
+form.append(button);
 document.body.append(result);
 
-button.addEventListener('click', function funcCallback() {
+form.addEventListener('submit', function funcCallback(event) {
+    event.preventDefault();
     if(word.textContent[word.textContent.length - 1] === input.value[0]) {
         result.textContent = 'Success';
         word.textContent = input.value;
         input.value = '';
+        input.focus();
     } else {
         result.textContent = 'Fail';
+        input.value = '';
+        input.focus();
     }
 })
