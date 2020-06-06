2a5fcef9ad4e4376737585fa01adf562e5801907
J3SUNG <wptjd6141@naver.com>
2020-06-06 19:32:34 +0900

    리팩토링

--- 자스스톤/자스스톤.js
+++ 자스스톤/자스스톤.js
@@ -1,25 +1,32 @@
-var myHero = document.getElementById("my-hero");
-var rivalHero = document.getElementById("rival-hero");
-var myDeck = document.getElementById("my-deck");
-var rivalDeck = document.getElementById("rival-deck");
-var myField = document.getElementById("my-cards");
-var rivalField = document.getElementById("rival-cards");
-var myCost = document.getElementById("my-cost");
-var rivalCost = document.getElementById("rival-cost");
+var my = {
+  hero: document.getElementById("my-hero"),
+  deck: document.getElementById("my-deck"),
+  field: document.getElementById("my-cards"),
+  cost: document.getElementById("my-cost"),
+  deckData: [],
+  heroData: [],
+  fieldData: [],
+};
+
+var rival = {
+  hero: document.getElementById("rival-hero"),
+  deck: document.getElementById("rival-deck"),
+  field: document.getElementById("rival-cards"),
+  cost: document.getElementById("rival-cost"),
+  deckData: [],
+  heroData: [],
+  fieldData: [],
+};
+
 var turnButton = document.getElementById("turn-btn");
-var myDeckData = [];
-var rivalDeckData = [];
-var myHeroData;
-var rivalHeroData;
-var myFieldData = [];
-var rivalFieldData = [];
+var turn = true;
 
 function set(data, dom, hero) {
   var card = document.querySelector(".card-hidden .card").cloneNode(true);
   card.querySelector(".card-cost").textContent = data.cost;
   card.querySelector(".card-att").textContent = data.att;
   card.querySelector(".card-hp").textContent = data.hp;
-  myDeck.appendChild(card);
+  my.deck.appendChild(card);
   if (hero) {
     card.querySelector(".card-cost").style.display = "none";
     var name = document.createElement("div");
@@ -29,49 +36,47 @@ function set(data, dom, hero) {
   card.addEventListener("click", function () {
     if (turn) {
       if (!data.mine || data.field) {
-        console.log(data.mine);
-        console.log(turn);
         return;
       }
-      var currentCost = Number(myCost.textContent);
-      if (Number(myCost.textContent) < data.cost) {
+      var currentCost = Number(my.cost.textContent);
+      if (Number(my.cost.textContent) < data.cost) {
         return;
       }
-      var idx = myDeckData.indexOf(data);
-      myDeckData.splice(idx, 1);
-      myFieldData.push(data);
-      myDeck.innerHTML = "";
-      myField.innerHTML = "";
-      myFieldData.forEach(function (data) {
-        set(data, myField);
+      var idx = my.deckData.indexOf(data);
+      my.deckData.splice(idx, 1);
+      my.fieldData.push(data);
+      my.deck.innerHTML = "";
+      my.field.innerHTML = "";
+      my.fieldData.forEach(function (data) {
+        set(data, my.field);
       });
-      myDeckData.forEach(function (data) {
-        set(data, myDeck);
+      my.deckData.forEach(function (data) {
+        set(data, my.deck);
       });
       data.field = true;
-      myCost.textContent = currentCost - data.cost;
+      my.cost.textContent = currentCost - data.cost;
       createMyDeck(1);
     } else {
       if (data.mine || data.field) {
         return;
       }
-      var currentCost = Number(rivalCost.textContent);
-      if (Number(rivalCost.textContent) < data.cost) {
+      var currentCost = Number(rival.cost.textContent);
+      if (Number(rival.cost.textContent) < data.cost) {
         return;
       }
-      var idx = rivalDeckData.indexOf(data);
-      rivalDeckData.splice(idx, 1);
-      rivalFieldData.push(data);
-      rivalDeck.innerHTML = "";
-      rivalField.innerHTML = "";
-      rivalFieldData.forEach(function (data) {
-        set(data, rivalField);
+      var idx = rival.deckData.indexOf(data);
+      rival.deckData.splice(idx, 1);
+      rival.fieldData.push(data);
+      rival.deck.innerHTML = "";
+      rival.field.innerHTML = "";
+      rival.fieldData.forEach(function (data) {
+        set(data, rival.field);
       });
-      rivalDeckData.forEach(function (data) {
-        set(data, rivalDeck);
+      rival.deckData.forEach(function (data) {
+        set(data, rival.deck);
       });
       data.field = true;
-      rivalCost.textContent = currentCost - data.cost;
+      rival.cost.textContent = currentCost - data.cost;
       createRivalDeck(1);
     }
   });
@@ -79,31 +84,31 @@ function set(data, dom, hero) {
 }
 function createMyDeck(num) {
   for (var i = 0; i < num; ++i) {
-    myDeckData.push(createCard(false, true));
+    my.deckData.push(createCard(false, true));
   }
-  myDeck.innerHTML = "";
-  myDeckData.forEach(function (data) {
-    set(data, myDeck);
+  my.deck.innerHTML = "";
+  my.deckData.forEach(function (data) {
+    set(data, my.deck);
   });
 }
 function createRivalDeck(num) {
   for (var i = 0; i < num; ++i) {
-    rivalDeckData.push(createCard(false, false));
+    rival.deckData.push(createCard(false, false));
   }
-  rivalDeck.innerHTML = "";
-  rivalDeckData.forEach(function (data) {
-    set(data, rivalDeck);
+  rival.deck.innerHTML = "";
+  rival.deckData.forEach(function (data) {
+    set(data, rival.deck);
   });
 }
 function createMyHero() {
-  myHeroData = createCard(true, true);
-  set(myHeroData, myHero, true);
+  my.heroData = createCard(true, true);
+  set(my.heroData, my.hero, true);
 }
 function createRivalHero() {
-  rivalHeroData = createCard(true, false);
-  set(rivalHeroData, rivalHero, true);
+  rival.heroData = createCard(true, false);
+  set(rival.heroData, rival.hero, true);
 }
-function Card(hero, myCard) {
+function Card(hero, card) {
   if (hero) {
     this.att = Math.ceil(Math.random() * 2);
     this.hp = Math.ceil(Math.random() * 5) + 25;
@@ -113,9 +118,7 @@ function Card(hero, myCard) {
     this.hp = Math.ceil(Math.random() * 5);
     this.cost = Math.floor((this.att + this.hp) / 2);
   }
-  console.log(myCard);
-  if (myCard) {
-    console.log("AA");
+  if (card) {
     this.mine = true;
   }
 }
@@ -132,15 +135,14 @@ function init() {
 turnButton.addEventListener("click", function () {
   turn = !turn;
   if (turn) {
-    rivalCost.textContent = 10;
+    rival.cost.textContent = 10;
   } else {
-    myCost.textContent = 10;
+    my.cost.textContent = 10;
   }
   document.getElementById("rival").classList.toggle("turn");
   document.getElementById("my").classList.toggle("turn");
 });
 
-var turn = true;
 function getCard() {}
 
 function page() {}
