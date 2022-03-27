a744da41899e3c99374164bb10ba762b662231ff
J3SUNG <wptjd6141@naver.com>
2022-03-28 03:15:12 +0900

    check type

--- JavaScript Study/Nomad_Chrome_App/app.js
+++ JavaScript Study/Nomad_Chrome_App/app.js
@@ -1,6 +1,14 @@
 const a = 5;
 let b = 4;
+const type1 = true;
+const type2 = false;
+const type3 = null;
+const type4 = undefined;
 
+console.log(type1);
+console.log(type2);
+console.log(type3);
+console.log(type4);
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
