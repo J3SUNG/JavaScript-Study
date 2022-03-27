1748311f4bbc9f2b065a235c545a57057d12e2f9
J3SUNG <wptjd6141@naver.com>
2022-03-28 04:10:39 +0900

    type

--- JavaScript Study/Nomad_Chrome_App/app.js
+++ JavaScript Study/Nomad_Chrome_App/app.js
@@ -1,14 +1,26 @@
 const a = 5;
 let b = 4;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
 const type1 = true;
 const type2 = false;
-const type3 = null;
-const type4 = undefined;
+const type3 = null; // 비어있다
+const type4 = undefined; // 정의되지 않았다
 
 console.log(type1);
 console.log(type2);
 console.log(type3);
 console.log(type4);
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const nonsense = [1, true, null, undefined, "Hey", false];
+
+console.log(daysOfWeek, nonsense);
+console.log(daysOfWeek[3]);
+
+nonsense.push("New");
+
+console.log(nonsense);
