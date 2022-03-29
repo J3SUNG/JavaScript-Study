2a5dcb02b85e060621097af9816985f2f2aeff7d
J3SUNG <wptjd6141@naver.com>
2022-03-29 23:17:08 +0900

    js

--- JavaScript Study/Nomad_Chrome_App/app.js
+++ JavaScript Study/Nomad_Chrome_App/app.js
@@ -7,8 +7,8 @@ console.log(a / b);
 
 const type1 = true;
 const type2 = false;
-const type3 = null; // 비어있다
-const type4 = undefined; // 정의되지 않았다
+const type3 = null; // 정의되어있다 비어있다
+const type4 = undefined; // 정의되지않았다
 
 console.log(type1);
 console.log(type2);
