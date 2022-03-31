d22d49216410f7ceacefcd65a41b8d9450fa0b50
J3SUNG <wptjd6141@naver.com>
2022-03-31 23:57:48 +0900

    conditional

--- JavaScript Study/Nomad_Chrome_App/practice3.js
+++ JavaScript Study/Nomad_Chrome_App/practice3.js
@@ -1,3 +1,9 @@
 const age = prompt("How old are you?");
 
-console.log(age);
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else if (age < 17) {
+  console.log("You are too young.");
+} else {
+  console.log("You can drink");
+}
