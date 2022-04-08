4a3f8d3f72b53883dfb6c50299a4647f08226dc5
J3SUNG <wptjd6141@naver.com>
2022-04-09 00:21:55 +0900

    querySelector

--- JavaScript Study/Nomad_Chrome_App/practice4.js
+++ JavaScript Study/Nomad_Chrome_App/practice4.js
@@ -3,3 +3,17 @@ const title = document.getElementById("title");
 console.dir(title);
 
 title.innerText += " inner Text";
+
+const hellos = document.getElementsByClassName("hello");
+
+console.log(hellos);
+
+const title2 = document.querySelector("title");
+
+console.dir(title2);
+
+title2.innerText += " inner Text";
+
+const hellos2 = document.getElementsByClassName("hello");
+
+console.log(hellos2);
