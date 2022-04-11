f904d2c2a10c90541866c500f5c21c49b77f109d
J3SUNG <wptjd6141@naver.com>
2022-04-11 22:59:24 +0900

    js

--- JavaScript Study/Nomad_Chrome_App/practice6.js
+++ JavaScript Study/Nomad_Chrome_App/practice6.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-// function handleTitleClick() { //togle
+// function handleTitleClick() { // togle
 //   const clickedClass = "clicked";
 //   if (h1.classList.contains(clickedClass)) {
 //     h1.classList.remove(clickedClass);
