320723c9f6b93fcb123555b2ddd9b8256d6893f8
J3SUNG <wptjd6141@naver.com>
2022-04-15 03:29:06 +0900

    clock

---
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("h2#clock");
+
+// clock.innerText = "Hey";
+function getClock() {
+  const date = new Date();
+  let hour = String(date.getHours()).padStart(2, "0");
+  let minute = String(date.getMinutes()).padStart(2, "0");
+  let second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hour}:${minute}:${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
