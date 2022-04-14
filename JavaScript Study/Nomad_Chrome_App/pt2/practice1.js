8d1da70ecdeddb24c9c9c38fbd2199bc2d8366cb
J3SUNG <wptjd6141@naver.com>
2022-04-14 16:58:09 +0900

    pt2

---
+++ JavaScript Study/Nomad_Chrome_App/pt2/practice1.js
@@ -0,0 +1,9 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  console.log("Hello " + username);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
