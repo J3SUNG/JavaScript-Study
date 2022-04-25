ff741910a1e3bb79790f8c521558b7118c4dc4bd
J3SUNG <wptjd6141@naver.com>
2022-04-25 22:02:29 +0900

    js

--- JavaScript Study/Nomad_Chrome_App/pt2/js/clock.js
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/clock.js
@@ -6,7 +6,7 @@ function getClock() {
   let hour = String(date.getHours()).padStart(2, "0");
   let minute = String(date.getMinutes()).padStart(2, "0");
   let second = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hour}:${minute}:${second}`;
+  clock.innerText = `${hour} : ${minute} : ${second}`;
 }
 
 getClock();
