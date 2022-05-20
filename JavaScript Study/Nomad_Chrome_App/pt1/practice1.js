3e196b365250cbd4fa514284927da5826239cf86
J3SUNG <wptjd6141@naver.com>
2022-05-20 23:10:37 +0900

    js

--- JavaScript Study/Nomad_Chrome_App/pt1/practice1.js
+++ JavaScript Study/Nomad_Chrome_App/pt1/practice1.js
@@ -7,8 +7,8 @@ console.log(a / b);
 
 const type1 = true;
 const type2 = false;
-const type3 = null; // 정의되어있다, 비어있다
-const type4 = undefined; // 정의되지않았다
+const type3 = null; // 정의 되어 있다, 비어있다
+const type4 = undefined; // 정의 되지 않았다
 
 console.log(type1);
 console.log(type2);
