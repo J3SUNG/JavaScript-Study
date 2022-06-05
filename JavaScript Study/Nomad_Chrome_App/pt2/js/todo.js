22bcc1014444bd0fae17324a5e0d689521e39f7f
J3SUNG <wptjd6141@naver.com>
2022-06-06 02:57:49 +0900

    add list

--- JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
