905ba6ab91ed18a5611e1d929e16b10d99e4cbaa
J3SUNG <wptjd6141@naver.com>
2022-06-06 03:06:18 +0900

    delete todo

--- JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newToDo + " ";
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  span.innerText = newToDo;
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
