dffbae380e7ca5459ffc7aebef3f94da6b886e82
J3SUNG <wptjd6141@naver.com>
2022-06-13 21:53:52 +0900

    js

--- JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newToDo) {
   const span = document.createElement("span");
   span.innerText = newToDo.text + " ";
   const button = document.createElement("button");
-  button.innerText = "Remove";
+  button.innerText = "remove";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
