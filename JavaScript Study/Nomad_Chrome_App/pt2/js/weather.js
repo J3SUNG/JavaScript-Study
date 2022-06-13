111a39801d836d4a995edb2c57093870e1219ebf
J3SUNG <wptjd6141@naver.com>
2022-06-14 05:04:58 +0900

    weather

---
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "114434551f59912465565c1f88d4157c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+  console.log(url);
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
