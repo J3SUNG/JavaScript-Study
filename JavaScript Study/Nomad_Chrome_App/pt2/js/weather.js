c94d657acc4441a08aa6c182a0b42a413e06c230
J3SUNG <wptjd6141@naver.com>
2022-06-21 23:49:19 +0900

    JS

--- JavaScript Study/Nomad_Chrome_App/pt2/js/weather.js
+++ JavaScript Study/Nomad_Chrome_App/pt2/js/weather.js
@@ -17,7 +17,7 @@ function onGeoOk(position) {
 }
 
 function onGeoError() {
-  alert("Can't find you! No weather for you.");
+  alert("Can't find you! No weather for you. ");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
