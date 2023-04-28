3ce7cb52101856c407c7834d4aa403e8dfdea116
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-28 23:25:03 +0900

    js

--- JavaScript Study/Yalco/hi.js
+++ JavaScript Study/Yalco/hi.js
@@ -3,14 +3,6 @@ console.log("Hello, JS!");
 console.log("run extension");
 console.log(`ctrl + art + n`);
 
-const str = new String("abcde");
-const num = new Number(123.4567);
-const bool = new Boolean(true);
-
-console.log(str.valueOf());
-console.log(num.valueOf());
-console.log(bool.valueOf());
-
 const arrLike = {
   0: "🍎",
   1: "🍌",
