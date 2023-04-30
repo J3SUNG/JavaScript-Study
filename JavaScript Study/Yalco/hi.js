5c7246f1ff74625ebc978d5339b5ba5593bae153
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-30 14:49:49 +0900

    js

--- JavaScript Study/Yalco/hi.js
+++ JavaScript Study/Yalco/hi.js
@@ -1,35 +1,3 @@
-console.log("Hello, JS!");
-
-console.log("run extension");
-console.log(`ctrl + art + n`);
-
-const arrLike = {
-  0: "🍎",
-  1: "🍌",
-  2: "🥝",
-  3: "🍒",
-  4: "🫐",
-  length: 5,
-};
-
-for (let i = 0; i < arrLike.length; i++) {
-  console.log(arrLike[i]);
-}
-
-for (const item of arrLike) {
-  console.log(item);
-}
-
-for (const item of Array.from(arrLike)) {
-  console.log(item);
-}
-
-const arr1 = [1, 2, 3];
-const arr2 = Array.from(arr1);
-arr2.push(4);
-
-console.log(arr1, arr2);
-
 function classIntro(classNo, teacher, ...children) {
   return `${classNo}반의 선생님은 ${teacher}, ` + `학생들은 ${children.join(", ")}입니다.`;
 }
@@ -39,3 +7,12 @@ const teacher = "김민지";
 const students = ["영희", "철수", "보라", "돌준", "달숙"];
 
 console.log(classIntro(classNo, teacher, ...students));
+
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1];
+
+console.log(arr1 === arr2);
+
+arr1[0] = 0;
+
+console.log(arr1, arr2);
