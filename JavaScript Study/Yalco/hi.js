6dc7947ac0d128ffc63efababcf0da17f818c93b
wptjd6141@naver.com <wptjd6141@naver.com>
2023-04-30 14:49:35 +0900

    js

--- JavaScript Study/Yalco/hi.js
+++ JavaScript Study/Yalco/hi.js
@@ -29,3 +29,13 @@ const arr2 = Array.from(arr1);
 arr2.push(4);
 
 console.log(arr1, arr2);
+
+function classIntro(classNo, teacher, ...children) {
+  return `${classNo}반의 선생님은 ${teacher}, ` + `학생들은 ${children.join(", ")}입니다.`;
+}
+
+const classNo = 3;
+const teacher = "김민지";
+const students = ["영희", "철수", "보라", "돌준", "달숙"];
+
+console.log(classIntro(classNo, teacher, ...students));
