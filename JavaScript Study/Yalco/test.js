6c744bc7947f115d287a606bd23e768e799a70fe
wptjd6141@naver.com <wptjd6141@naver.com>
2023-05-04 00:57:11 +0900

    js

--- JavaScript Study/Yalco/test.js
+++ JavaScript Study/Yalco/test.js
@@ -4,3 +4,19 @@ const myGlobalConst = 1;
 function myGlobalFunc() {}
 
 console.log(globalThis.myGlobalVar, globalThis.myGlobalConst, globalThis.myGlobalFunc);
+
+errorFuncs.forEach((func) => {
+  try {
+    func();
+  } catch (e) {
+    if (e instanceof TypeError) {
+      console.error("자료형 확인하세요.");
+      return;
+    }
+    if (e instanceof ReferenceError) {
+      console.error("선언 안 된 거 쓴 거 없는지 확인하세요.");
+      return;
+    }
+    console.error("아니, 뭘 한 거에요?");
+  }
+});
