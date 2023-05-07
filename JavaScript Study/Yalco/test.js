be521f7a83121eae0fb74899b2cab25aa1f1ddef
wptjd6141@naver.com <wptjd6141@naver.com>
2023-05-07 22:30:11 +0900

    promise

--- JavaScript Study/Yalco/test.js
+++ JavaScript Study/Yalco/test.js
@@ -1,6 +1,20 @@
-// 💡 결과가 Promise의 인스턴스임 확인
-console.log(fetch("https://showcases.yalco.kr/javascript/mockserver/race-result"));
+const SERVER_URL = "https://showcases.yalco.kr/javascript/mockserver/";
 
-fetch("https://showcases.yalco.kr/javascript/mockserver/race-result")
-  .then((response) => response.json())
-  .then(console.log);
+fetch(SERVER_URL + "race-result")
+  .then((result) => result.json())
+  .then((arry) => {
+    return arry.sort((a, b) => {
+      return a.record - b.record;
+    })[0].runner_idx;
+  })
+  .then((winnerIdx) => {
+    return fetch(`${SERVER_URL}runners/${winnerIdx}`);
+  })
+  .then((result) => result.json())
+  .then(({ school_idx }) => school_idx)
+  .then((schoolIdx) => {
+    return fetch(`${SERVER_URL}schools/${schoolIdx}`);
+  })
+  .then((result) => result.json())
+  .then(console.log)
+  .catch(console.error);
