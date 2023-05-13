5abfc1eaff54dfa9f9212bb349bd13b891491263
wptjd6141@naver.com <wptjd6141@naver.com>
2023-05-13 19:34:46 +0900

    js

--- JavaScript Study/Yalco/hi.js
+++ JavaScript Study/Yalco/hi.js
@@ -1,9 +1,6 @@
 const orgArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const arr1 = [...orgArr.slice(0, 3), ...orgArr.slice(6, 9)];
 console.log(arr1);
-
 const arr2 = orgArr.filter((_, i) => !(i >= 3 && i < 6));
 console.log(arr2);
-
 console.log(orgArr);
